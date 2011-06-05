f7c6b786a4354064748c3fe2996a099e961b4d76
zeroreality@gmail.com <zeroreality@gmail.com@cfffa0f3-a2ed-bcc4-04c0-aa62a5bee674>
2011-06-05 16:45:16 +0000

    Initial copyright and license header.

---
+++ pseudo.js
@@ -0,0 +1,7 @@
+/* ---------------------------------------------------------------------------
+ * Pseudo JavaScript framework, version 2.0.0
+ * (c) 2011 Alex Lein
+ *
+ * Pseudo is freely distributable under the terms of an MIT-style license.
+ * For details, see http://www.opensource.org/licenses/mit-license.php
+ *--------------------------------------------------------------------------*/
