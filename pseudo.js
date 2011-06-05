5ed32f1dd29a36fee5a74e3a8872ae1610b8d97c
zeroreality@gmail.com <zeroreality@gmail.com@cfffa0f3-a2ed-bcc4-04c0-aa62a5bee674>
2011-06-05 16:23:19 +0000

    Initial header

---
+++ pseudo.js
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------------------------------
+ *  Pseudo JavaScript framework, version 1.2
+ *  (c) 2009 Alex Lein
+ *
+ *  Pseudo is based heavily on the awesome
+ *  Prototype JavaScript framework (c) 2005-2009 Sam Stephenson
+ *
+ *  Pseudo is freely distributable under the terms of an MIT-style license.
+ *  For details, see http://www.opensource.org/licenses/mit-license.php
+ *--------------------------------------------------------------------------*/
+
