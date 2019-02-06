86cdd0ffe23f6a08e7923a100c49c44291363920
Alex Lein <alein@trakitwireless.com>
2019-02-06 13:30:58 -0500

    Updated the json util with the pretty-print JSON output helper function

--- src/util/json.js
+++ src/util/json.js
@@ -1,2 +1,59 @@
-﻿/// <reference path="..\blds\pseudo3.js" />
+﻿/// <reference path="..\..\blds\pseudo3.js" />
 "use strict";
+
+/**
+ * Makes the JSON pretty
+ * @param {?} object
+ * @param {!string} tab
+ * @param {!number} depth
+ * @return {!string}
+ */
+function JSON_PRETTY(object, tab, depth) {
+	var html = [];
+	if (OBJECT_IS_STRING(object)) {
+		//	if (depth) html.push(tab.repeat(depth));
+		html.push("<span class='json-string'>\"" + object + "\"</span>");
+	} else if (OBJECT_IS_NUMBER(object)) {
+		//	if (depth) html.push(tab.repeat(depth));
+		html.push("<span class='json-num'>" + object + "</span>");
+	} else if (OBJECT_IS_BOOLEAN(object)) {
+		//	if (depth) html.push(tab.repeat(depth));
+		html.push("<span class='json-bool'>" + object + "</span>");
+	} else if (OBJECT_IS_OBJECT(object)) {
+		if (!OBJECT_IS_STRING(tab)) tab = "\t";
+		if (!OBJECT_IS_NUMBER(depth)) depth = 0;
+		var indent = depth ? tab.repeat(depth) : "";
+		if (object instanceof Array) {
+			html.push("<span class='json-array'>[");
+			if (object.length) {
+				html.push("<br/>");
+				html.push(object.map(function(obj) {
+					return indent + tab + JSON_PRETTY(obj, tab, depth + 1);
+				}).join(",<br/>"));
+				html.push("<br/>" + indent);
+			}
+			html.push("]</span>");
+		} else if (object) {
+			html.push("<span class='json-object'>{");
+			var keys = GET_KEYS(object);
+			if (keys.length) {
+				html.push("<br/>");
+				html.push(keys.map(function(each) {
+					return indent + tab
+						+ "<span class='json-key'>\"" + each + "\"</span>: "
+						+ JSON_PRETTY(object[each], tab, depth + 1);
+				}).join(",<br/>"));
+				html.push("<br/>" + indent);
+			}
+			html.push("}</span>");
+		} else {
+			html.push("<span class='json-null'>null</span>");
+		}
+	}
+	return html.join("");
+}
+
+/** @expose */
+ns.JSON = {
+	"pretty": JSON_PRETTY,
+};
