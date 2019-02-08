2887d2c37a07ed7530a89294c2b82862a31a7a89
Alex Lein <alein@trakitwireless.com>
2019-02-08 14:59:28 -0500

    Updated build process with easier batch file

--- blds/closure.bat
+++ blds/closure.bat
@@ -1,29 +1,24 @@
-﻿
-copy "_header.txt" ^
-     + "..\src\_include.js" ^
-     + "..\src\objects\pseudo.js" ^
-     + "..\src\objects\Boolean.js" ^
-     + "..\src\objects\Number.js" ^
-     + "..\src\objects\Object.js" ^
-     + "..\src\objects\Array.js" ^
-     + "..\src\objects\Date.js" ^
-     + "..\src\objects\String.js" ^
-     + "..\src\objects\Function.js" ^
-     + "..\src\dom\_include.js" ^
-     + "..\src\dom\query.js" ^
-     + "..\src\dom\tree.js" ^
-     + "..\src\dom\events.js" ^
-     + "..\src\util\cookie.js" ^
-     + "..\src\util\css.js" ^
-     + "..\src\util\forms.js" ^
-     + "..\src\util\keyboard.js" ^
-     + "..\src\util\processor.js" ^
-     + "..\src\util\json.js" ^
-     + "..\src\util\protocol.js" ^
-	+ "_footer.txt" ^
-	"..\out\tmp"
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
 
-java -jar "closure.jar" --charset UTF-8 ^
+set /p build=Enter filename of build (leave blank for full build):
+if [!build!] == [] set build=pseudo3
+echo Building !build!.
+
+rem ### concat files together from build
+set merge="_header.txt"
+for /f "usebackq tokens=3" %%a in (`"findstr path= !build!.js"`) do (
+set file=%%a
+set merge=!merge! + !file:~5,100!
+rem echo !file!
+)
+set merge=!merge! + "_footer.txt"
+rem ### copy temp file to output folder
+copy !merge! "..\out\tmp"
+
+rem ### run Closure (update this path to compile)
+java -jar "..\path\to\closure.jar"
+                        --charset UTF-8 ^
                         --env BROWSER ^
                         --strict_mode_input ^
                         --language_in ECMASCRIPT5_STRICT ^
@@ -36,4 +31,6 @@ java -jar "closure.jar" --charset UTF-8 ^
                         --formatting SINGLE_QUOTES ^
                         --js_output_file "..\out\pseudo3.js" ^
                         "..\out\tmp"
+
+rem ### cleanup temp file
 del "..\out\tmp"
