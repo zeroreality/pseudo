cbfe0290670736f7110ed826d416e8b1aca2db94
Alex Lein <alein@trakitwireless.com>
2020-03-16 15:52:23 -0400

    Updated pseudo3.JSON.pretty to actually work properly

--- src/util/json.js
+++ src/util/json.js
@@ -9,45 +9,59 @@
  */
 function JSON_PRETTY(object, tab, depth) {
 	var html = [];
-	if (OBJECT_IS_STRING(object)) {
-		//	if (depth) html.push(tab.repeat(depth));
-		html.push("<span class='json-string'>\"" + object + "\"</span>");
-	} else if (OBJECT_IS_NUMBER(object)) {
-		//	if (depth) html.push(tab.repeat(depth));
-		html.push("<span class='json-num'>" + object + "</span>");
-	} else if (OBJECT_IS_BOOLEAN(object)) {
-		//	if (depth) html.push(tab.repeat(depth));
-		html.push("<span class='json-bool'>" + object + "</span>");
-	} else if (OBJECT_IS_OBJECT(object)) {
-		if (!OBJECT_IS_STRING(tab)) tab = "\t";
-		if (!OBJECT_IS_NUMBER(depth)) depth = 0;
-		var indent = depth ? tab.repeat(depth) : "";
-		if (object instanceof Array) {
-			html.push("<span class='json-array'>[");
+	switch (PSEUDO_KLASS_NAME(object)) {
+		case "null":
+			html.push('<span class="json-null">' + object + '</span>');
+			break;
+		case "Boolean":
+			html.push('<span class="json-bool">' + object + '</span>');
+			break;
+		case "Number":
+			html.push('<span class="json-num">' + object + '</span>');
+			break;
+		case "String":
+			html.push('<span class="json-string">"' + object + '"</span>');
+			break;
+		case "Array":
+			if (!OBJECT_IS_STRING(tab)) tab = "\t";
+			if (!OBJECT_IS_NUMBER(depth)) depth = 0;
+			html.push('<span class="json-array">[');
 			if (object.length) {
-				html.push("<br/>");
+				var indent = depth
+					? tab.repeat(depth)
+					: "";
+				html.push('<br/>');
 				html.push(object.map(function(obj) {
 					return indent + tab + JSON_PRETTY(obj, tab, depth + 1);
-				}).join(",<br/>"));
-				html.push("<br/>" + indent);
+				}).join(',<br/>'));
+				html.push('<br/>' + indent);
 			}
-			html.push("]</span>");
-		} else if (object) {
-			html.push("<span class='json-object'>{");
+			html.push(']</span>');
+			break;
+		case "Object":
+			if (!OBJECT_IS_STRING(tab)) tab = "\t";
+			if (!OBJECT_IS_NUMBER(depth)) depth = 0;
 			var keys = GET_KEYS(object);
+			html.push('<span class="json-object">{');
 			if (keys.length) {
-				html.push("<br/>");
+				var indent = depth
+					? tab.repeat(depth)
+					: "";
+				html.push('<br/>');
 				html.push(keys.map(function(each) {
 					return indent + tab
-						+ "<span class='json-key'>\"" + each + "\"</span>: "
+						+ '<span class="json-key">"' + each + '"</span>: '
 						+ JSON_PRETTY(object[each], tab, depth + 1);
-				}).join(",<br/>"));
-				html.push("<br/>" + indent);
+				}).join(',<br/>'));
+				html.push('<br/>' + indent);
 			}
-			html.push("}</span>");
-		} else {
-			html.push("<span class='json-null'>null</span>");
-		}
+			html.push('}</span>');
+			break;
+		case "undefined":
+		default:
+			// for non-JSON types like: undefined, Date, etc...
+			html.push('<span class="json-invalid">' + object + '</span>');
+			break;
 	}
 	return html.join("");
 }
