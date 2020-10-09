6dfac1933cfb2080a2609d2de6f75987515ed1fe
Alex Lein <alein@trakitwireless.com>
2020-10-09 08:03:31 -0400

    This was a mistake

--- src/objects/Function.js
+++ src/objects/Function.js
@@ -18,12 +18,3 @@ Function_prototype.defer = function(var_args) {
 	var_args = SLICE.call(arguments);
 	return SET_TIMER(function() { return method.apply(WIN, var_args) }, 1);
 };
-/**
- * Returns a Promise using this function as the executor
- * @this {Function}
- * @expose
- * @return {Promise}
- */
-Function_prototype.promise = function() {
-	return new Promise(this);
-};
