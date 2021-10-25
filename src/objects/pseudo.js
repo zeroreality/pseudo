84e32464756388c1e7a2def491821a9ac48d6b4d
Alex Lein <alein@trakitwireless.com>
2021-10-25 13:21:06 -0400

    These items were moved to the DOM include file

--- src/objects/pseudo.js
+++ src/objects/pseudo.js
@@ -2,56 +2,6 @@
 
 /** @const */
 var PSEUDO_FILTER_CLASS = /^\[object\s(.*)\]$/;
-/** @private */
-var DOC_HEAD = DOC.getElementsByTagName("head")[0] || DOC_EL.appendChild(DOC.createElement("head"));
-/**
- * Removes a script or stylesheet from the document
- * @param {string} tag
- * @param {string} attr
- * @param {string} source
- */
-function PSEUDO_RELOAD(tag, attr, source) {
-	var nodes = DOC.getElementsByTagName(tag), node = null;
-	for (var i = 0; node = nodes[i]; i++) {
-		if (node.getAttribute(attr) === source) {
-			return node.parentNode.removeChild(node);
-		}
-	}
-}
-/**
- * Adds a script to the document
- * @param {string} source
- * @param {Function} callback
- * @param {boolean} reload
- * @return {HTMLScriptElement}
- */
-function PSEUDO_ADD_SCRIPT(source, callback, reload) {
-	if (reload) PSEUDO_RELOAD("script", "src", source);
-
-	var file = DOC.createElement("script");
-	file.async = false;	// html5/IE10
-	file.type = "text/javascript";
-	file.setAttribute("src", source);
-	if (callback) file.onload = callback;
-	return DOC_HEAD.appendChild(file);
-}
-/**
- * Adds a stylesheet to the document
- * @param {string} source
- * @param {string} media
- * @param {boolean} reload
- * @return {HTMLLinkElement}
- */
-function PSEUDO_ADD_SHEET(source, media, reload) {
-	if (reload) PSEUDO_RELOAD("link", "href", source);
-
-	var file = DOC.createElement("link");
-	file.setAttribute("rel", "stylesheet");
-	file.setAttribute("type", "text/css");
-	file.setAttribute("href", source);
-	if (media) file.setAttribute("media", media);
-	return DOC_HEAD.appendChild(file);
-}
 /**
  * Creates an HTTP safe string for transmission similar to the {@link window.escape} function.
  * @param {string} input
@@ -69,23 +19,6 @@ function PSEUDO_ESCAPE(input) {
 	});
 }
 
-/**
- * Returns a string identifying the browser.
- * @return {string|null}
- */
-function PSEUDO_BROWSER() {
-	return WIN["chrome"]
-		? !WIN["chrome"]["loadTimes"] //|| NAV.appVersion.search(/Edge?/) > -1
-			? "edge"
-			: "chrome"
-		: WIN["InstallTrigger"]
-			? "firefox"
-			: DOC["documentMode"]
-				? "ie"
-				: WIN["opera"]
-					? "opera"
-					: null;
-}
 /**
  * Does a deep copy/clone of the given object.  Automatically omits recursive branches.
  * @param {?} object			The object to copy/clone.
@@ -171,16 +104,6 @@ function PSEUDO_MERGE(var_args) {
 }
 
 /**
- * Adds a script to the document inside the <head> tag.
- * @expose
- **/
-ns.addScript = PSEUDO_ADD_SCRIPT;
-/**
- * Adds a link element for the given CSS file to the document inside the <head> tag.
- * @expose
- **/
-ns.addSheet = PSEUDO_ADD_SHEET;
-/**
  * Returns the class name of the given object like "Array", "Document", or "Number".
  * For null and undefined will return "null" and "undefined" respectively.
  * @expose
@@ -223,9 +146,3 @@ ns.isNaN = IS_NAN;
  * @expose
  **/
 ns.isN = IS_AN;
-/**
- * Returns a string identifying the browser.
- * If the browser cannot be identified, returns null.
- * @expose
- **/
-ns.checkBrowser = PSEUDO_BROWSER;
